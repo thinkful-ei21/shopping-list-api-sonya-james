981eb3cbe5d25279cbff6bb0f89f4d49b8383a65
James Stuckey <jstuckey@rutgers.edu>
2018-05-16 15:59:54 -0400

    implementing findAndUpdate function

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,11 @@ $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
   api.getItems((items) => {
+    const item = items[0];
+    console.log('current name: ' + item.name);
+    store.findAndUpdate(item.id, { name: 'foobar' });
+    console.log('new name: ' + item.name);
+
     items.forEach((item) => store.addItem(item));
     shoppingList.render();
   });
--- scripts/store.js
+++ scripts/store.js
@@ -2,53 +2,43 @@
 'use strict';
 // eslint-disable-next-line no-unused-vars
 const store = (function(){
-  const addItem = function(item) {
-    this.items.push(item);
-  };
+    const addItem = function(item) {
+        this.items.push(item);
+    };
 
-  const findById = function(id) {
-    return this.items.find(item => item.id === id);
-  };
+    const findById = function(id) {
+        return this.items.find(item => item.id === id);
+    };
 
-  const findAndToggleChecked = function(id) {
-    const item = this.findById(id);
-    item.checked = !item.checked;
-  };
+    const findAndDelete = function(id) {
+        this.items = this.items.filter(item => item.id !== id);
+    };
 
-  const findAndDelete = function(id) {
-    this.items = this.items.filter(item => item.id !== id);
-  };
+    const toggleCheckedFilter = function() {
+        this.hideCheckedItems = !this.hideCheckedItems;
+    };
 
-  const findAndUpdateName = function(id, name) {
-    try {
-      Item.validateName(name);
-      const item = this.findById(id);
-      item.name = name;
-    } catch(e) {
-      console.log('Cannot update name: ' + e.message);
+    const setSearchTerm = function(term) {
+        this.searchTerm = term;
+    };
+
+    function findAndUpdate(id, newData) {
+        console.log(this.items[id]);
+        const item = this.items.find(item => item.id === id);
+        
+        const updatedItem = Object.assign(newData, item);
     }
-  };
-
-  const toggleCheckedFilter = function() {
-    this.hideCheckedItems = !this.hideCheckedItems;
-  };
-
-  const setSearchTerm = function(term) {
-    this.searchTerm = term;
-  };
 
   return {
-    items: [],
-    hideCheckedItems: false,
-    searchTerm: '',
+        items: [],
+        hideCheckedItems: false,
+        searchTerm: '',
+        addItem,
+        findById,
+        findAndDelete,
+        toggleCheckedFilter,
+        setSearchTerm,
+        findAndUpdate,
+    };
 
-    addItem,
-    findById,
-    findAndToggleChecked,
-    findAndDelete,
-    findAndUpdateName,
-    toggleCheckedFilter,
-    setSearchTerm,
-  };
-  
 }());
